3d77b732c6f5fea519be1354f6df602de80c3d2d
RafaelFLopes <ferreiralopesrafael109@gmail.com>
2025-12-25 22:45:23 -0300

    calculadora imc utilizando metodo

---
+++ csharp-exercicios/imc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+class imc
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculando IMC");
+
+        Console.Write("Digite sua altura (ex: 1.75): ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite seu peso (ex: 84): ");
+        double p = double.Parse(Console.ReadLine());
+
+        double resultado = calculo(a, p);
+
+        Console.WriteLine("Seu IMC é: " + resultado);
+    }
+    static double calculo(double a, double p)
+    { 
+
+        double imc = p / (a * a);
+
+        return imc;
+    }
+}
--- csharp-exercicios/perguntandoNome.cs
+++ csharp-exercicios/perguntandoNome.cs
@@ -1,14 +1,14 @@
-﻿// Criando exercicios básico para compreender a sintaxe da linguagem C#
+﻿//// Criando exercicios básico para compreender a sintaxe da linguagem C#
 
-// Utilizando a classe Console para exibir uma mensagem no terminal e utilizando o método WriteLine para escrever uma linha de texto. 
-Console.WriteLine("Qual é o seu primeiro nome?");
+//// Utilizando a classe Console para exibir uma mensagem no terminal e utilizando o método WriteLine para escrever uma linha de texto. 
+//Console.WriteLine("Qual é o seu primeiro nome?");
 
-// Criando uma variável do tipo string para armazenar o nome do usuário, utilizando o método ReadLine para ler a entrada do usuário no terminal.
-string primeiroNome = Console.ReadLine();
+//// Criando uma variável do tipo string para armazenar o nome do usuário, utilizando o método ReadLine para ler a entrada do usuário no terminal.
+//string primeiroNome = Console.ReadLine();
 
-Console.WriteLine("Qual é o seu sobrenome?");
+//Console.WriteLine("Qual é o seu sobrenome?");
 
-string sobrenome = Console.ReadLine();
+//string sobrenome = Console.ReadLine();
 
-// Exibindo uma mensagem personalizada utilizando interpolação de strings para combinar o primeiro nome e o sobrenome do usuário.
-Console.WriteLine($"Olá, {primeiroNome} {sobrenome}! Seja bem-vindo(a)!");
+//// Exibindo uma mensagem personalizada utilizando interpolação de strings para combinar o primeiro nome e o sobrenome do usuário.
+//Console.WriteLine($"Olá, {primeiroNome} {sobrenome}! Seja bem-vindo(a)!");
