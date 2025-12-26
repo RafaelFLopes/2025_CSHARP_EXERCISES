4dbac66b19df69d2a86aa1ce2838d586f881a43a
RafaelFLopes <ferreiralopesrafael109@gmail.com>
2025-12-26 16:10:53 -0300

    CSHARP - exercicio

---
+++ csharp-exercicios/calculadoraUtilizandoMetodosEmUmArquivoDiferente/operacoes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercicios.calculadoraUtilizandoMetodosEmUmArquivoDiferente
+{
+    internal class operacoes
+    {
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtrair(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
--- csharp-exercicios/imc.cs
+++ csharp-exercicios/imc.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Reflection.PortableExecutable;
+﻿//using System;
 
-class imc
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Calculando IMC");
+//class imc
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Calculando IMC");
 
-        Console.Write("Digite sua altura (ex: 1.75): ");
-        double a = double.Parse(Console.ReadLine());
+//        Console.Write("Digite sua altura (ex: 1.75): ");
+//        double a = double.Parse(Console.ReadLine());
 
-        Console.Write("Digite seu peso (ex: 84): ");
-        double p = double.Parse(Console.ReadLine());
+//        Console.Write("Digite seu peso (ex: 84): ");
+//        double p = double.Parse(Console.ReadLine());
+        
+//        double resultado = calculo(a, p);
 
-        double resultado = calculo(a, p);
+//        Console.WriteLine("Seu IMC é: " + resultado);
+//    }
+//    static double calculo(double a, double p)
+//    { 
 
-        Console.WriteLine("Seu IMC é: " + resultado);
-    }
-    static double calculo(double a, double p)
-    { 
+//        double imc = p / (a * a);
 
-        double imc = p / (a * a);
-
-        return imc;
-    }
-}
+//        return imc;
+//    }
+//}
